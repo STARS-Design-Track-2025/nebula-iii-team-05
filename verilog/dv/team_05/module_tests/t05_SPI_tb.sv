6d1bbf58d6e871d3f9721899e429e8e492a862e6
Divine Testimony Pius <dpius@ecelnx17.ecn.purdue.edu>
2025-07-16 16:48:01 -0400

    Final draft

--- verilog/dv/team_05/module_tests/t05_SPI_tb.sv
+++ verilog/dv/team_05/module_tests/t05_SPI_tb.sv
@@ -1,2 +1,87 @@
-'timescale 1ms/10ns
-mdoule t05_SPI_tb; 
+`timescale 1ms/10ns
+module t05_SPI_tb; 
+    logic clk;
+    logic rst;
+    logic miso;
+    logic [7:0] read_output;
+    logic writebit;
+    logic read_en, write_en;
+    logic slave_select;
+    logic finish;
+    logic serialclk;
+
+    // Instantiate the SPI module
+    t05_SPI test(.clk(clk),
+                 .rst(rst),
+                 .miso(miso),
+                 .read_output(read_output),
+                 .writebit(writebit),
+                 .read_en(read_en),
+                 .write_en(write_en),
+                 .slave_select(slave_select),
+                 .finish(finish),
+                 .serial_clk(serialclk));
+
+    // Clock generation
+    always begin
+        #1 clk = ~clk; // 1ms clock period
+    end
+
+    always begin
+        #8 serialclk = 1;
+        #2 serialclk = 0;
+    end
+
+    // Testbench initial block
+    initial begin
+        $dumpfile("t05_SPI.vcd");
+        $dumpvars(0, t05_SPI_tb);
+
+        clk = 0;
+        serialclk = 0;
+        miso = 0;
+        serialclk = 0;
+        writebit = 0;
+        read_en = 0;
+        write_en = 0;
+        rst = 1; // Start with reset high
+        #2 
+        rst = 0; // Release reset after 2ms
+
+        // Test sequence
+        read_en = 0;
+        write_en = 0;
+        writebit = 0;
+
+        #300
+        #1032
+        miso = 1;
+        #8
+        miso = 0;
+
+        #912
+        miso = 1;
+        #8
+        miso = 0;
+        #242
+        miso = 1;
+        #20
+        miso = 0;
+        #10
+        miso = 1;
+        #8
+        miso = 0;  
+        #10
+        miso = 1;
+        #8
+        miso = 0;
+        #10
+        miso = 1;
+        #10
+        miso = 0;
+
+        #5000; // Run for 5ms
+
+        $finish; // End simulation
+    end
+endmodule
--- verilog/rtl/team_projects/team_05/src/t05_SPI.sv
+++ verilog/rtl/team_projects/team_05/src/t05_SPI.sv
@@ -10,10 +10,11 @@ module t05_SPI (
     input logic miso, // Read
     input logic rst, // Reset
     input logic serial_clk, clk,
-    input logic slave_select,
+    output logic slave_select,
     output logic [7:0] read_output,
     input logic writebit,
-    input logic read_en, write_en
+    input logic read_en, write_en,
+    output logic finish 
 );
 
 localparam
@@ -40,32 +41,59 @@ logic [5:0] timer_50, timer_50_n;
 logic [5:0] read_in_timer, read_in_timer_n;
 logic read_in_40, read_in_40_n;
 logic [31:0] read_address, read_address_n; 
-logic read_cmd_en, read_cmd_en_n; // Used to enable the read command
+logic read_cmd_en, read_cmd_en_n, write_cmd_en, write_cmd_en_n; // Used to enable the read command
 logic read_stop, read_stop_n; // Used to stop reading data
 logic cmd_en, cmd_en_n; // Used to enable the command
 
-// typedef enum logic [47:0] { 
-//     CMD0 = 48'b010000000000000000000000000000000000000010010101, // To go into IDLE STATE
-//     CMD58 = 48'b011110100000000000000000000000000000000001110101, // OCR Conditions
-//     CMD55 = 48'b011101110000000000000000000000000000000001100101, // Prepares the SD Card for the next command
-//     ACMD41 = 48'b011010010100000000000000000000000000000000000001, // Exits initialization mode and begn the data transfer
-//     CMD18 = {01010010, read_address, 00000001}, // Read multiple blocks until termination code
-//     CMD12 = 48'b010011000000000000000000000000000000000000000001, // Stop reading multiple blocks
-//     CMD24 = 48'b01011000, /* ARGUMENT ADDRESS */ //00000001 // Write single 
-//     CMD8 =  48'b010010000000000000000000000000011010101010000111 // Check the voltage range and if the card is compatible
-// }command_t;
-
-    always_ff @(posedge clk, posedge rst) begin
-        if (rst) begin
-            cmd_line <= CMD0; // Reset with CMD0
-            state <= INIT;
-            read_byte <= '0;
-        end else if (serial_clk) begin
-            cmd_line <= cmd_line_n;
-            state <= state_n;
-            read_byte <= read_byte_n;
-        end
+always_ff @(posedge clk, posedge rst) begin
+    if (rst) begin
+        cmd_line <= CMD0; // Reset with CMD0
+        state <= INIT;
+        read_byte <= '0;
+        idle_enable <= 0;
+        warmup_enable <= 1; // Enable warmup to stabilize the SD
+        response_holder <= '0;
+        response_enable <= 0;
+        enable_0 <= 0; // Enable the first bit of the command
+        enable_8 <= 0; // Enable the second bit of the command
+        enable_55 <= 0; // Enable the third bit of the command
+        enable_41 <= 0; // Enable the fourth bit of the command
+        read_in_40 <= 0; // Enable the read in 40 bits
+        read_in_timer <= 0; // Timer for reading in 40 bits
+        index_counter <= 0; // Counter for the number of bits received
+        timer_50 <= 0; // Timer for 50 clock cycles
+        read_cmd_en <= 0; // Enable the read command
+        read_stop <= 0; // Stop reading data
+        cmd_en <= 0; // Enable the command
+        warmup_counter <= 0; // Counter for the warmup
+        read_address <= '0; // Address for reading data
+        write_cmd_en <= 0; // Enable the write command
+        read_cmd_en <= 0; 
+    end else if (serial_clk) begin
+        cmd_line <= cmd_line_n;
+        state <= state_n;
+        read_byte <= read_byte_n;
+        idle_enable <= idle_enable_n;
+        warmup_enable <= warmup_enable_n; // Enable warmup to stabilize the SD
+        response_holder <= response_holder_n;
+        response_enable <= response_enable_n;
+        enable_0 <= enable_0_n; // Enable the first bit of the command
+        enable_8 <= enable_8_n; // Enable the second bit of the command
+        enable_55 <= enable_55_n; // Enable the third bit of the command
+        enable_41 <= enable_41_n; // Enable the fourth bit of the command
+        read_in_40 <= read_in_40_n; // Enable the read in 40 bits
+        read_in_timer <= read_in_timer_n; // Timer for reading in 40 bits
+        index_counter <= index_counter_n; // Counter for the number of bits received
+        timer_50 <= timer_50_n; // Timer for 50 clock cycles
+        read_cmd_en <= read_cmd_en_n; // Enable the read command
+        read_stop <= read_stop_n; // Stop reading data
+        cmd_en <= cmd_en_n; // Enable the command
+        warmup_counter <= warmup_counter_n; // Counter for the warmup
+        read_address <= read_address_n; // Address for reading data
+        write_cmd_en <= write_cmd_en_n; // Enable the write command
+        read_cmd_en <= read_cmd_en_n; // Enable the read command   
     end
+end
 
 // INIT Logic Block
 always_comb begin
@@ -90,6 +118,9 @@ always_comb begin
     read_output = '0;
     cmd_en_n = cmd_en;
     idle_enable_n = idle_enable;
+    write_cmd_en_n =  write_cmd_en;
+    slave_select = 0;
+    finish = 0;
 
     case (state)
         INIT: begin
@@ -106,7 +137,7 @@ always_comb begin
 
                 if(read_in_40) begin
                     response_holder_n = {response_holder[38:0], miso}; // Shift in data on MISO
-                    if(read_in_timer < 40) begin
+                    if(read_in_timer < 39) begin
                         read_in_timer_n = read_in_timer + 1;
                     end else begin
                         read_in_timer_n = 0;
@@ -115,84 +146,103 @@ always_comb begin
                     end
                 end
 
-                    // Response for CMD0 
+                // Response for CMD0 
                 if (response_enable) begin
                     response_enable_n = 0; 
                     if (response_holder[39:32] == 8'b00000001 && idle_enable) begin
                         enable_8_n = 1;
                         idle_enable_n = 0;
+                        response_holder_n = '0; // Reset the response holder after reading the response
                     end 
                     // Response for CMD8
-                    if (response_holder == 40'b000000100000000000000000000000110101010) begin
+                    else if (response_holder == 40'b000000100000000000000000000000110101010) begin
                         enable_55_n = 1;
+                        response_holder_n = '0; // Reset the response holder after reading the response
                     end
                     // Response for ACMD41
-                    if (response_holder[39:32] == 8'b00000000) begin
+                    else if (response_holder[39:32] == 8'b00000000) begin
                         state_n = READ;
+                        response_holder_n = '0; // Reset the response holder after reading the response
                     end
-                    if (response_holder[39:32] == 8'b00000001) begin
+                    else if (response_holder[39:32] == 8'b00000001) begin
                         enable_41_n = 0;
-                        enable_55_n = 1; 
+                        enable_55_n = 1;
+                        response_holder_n = '0; // Reset the response holder after reading the response 
                     end
                 end 
-                
-                else if (cmd_en) begin
-                    if (index_counter == 6'd48) begin
+
+                if (cmd_en) begin
+                    if (index_counter == 47) begin
                         index_counter_n = 0; // Reset the index counter after sending the command
                         cmd_en_n = 0;
                     end
-                    if (index_counter < 48) begin
+                    else if (index_counter < 47) begin
                         index_counter_n = index_counter + 1; // Increment the index counter for each bit
                     end
                     mosi = cmd_line[47 - index_counter]; // Shift out the command bit
                 end 
 
-                else if(enable_0) begin
+                if(enable_0) begin
                     cmd_line_n = CMD0; 
-                    cmd_en_n = 1;
                     if (timer_50 < 50) begin
                         timer_50_n = timer_50 + 1; // Increment the timer for 50 clock cycles
+                        if(timer_50 < 48) begin
+                            cmd_en_n = 1;
+                        end
                     end else begin
                         timer_50_n = 0; 
                         read_in_40_n = 1; 
                         enable_0_n = 0;
                         idle_enable_n = 1;
+                        cmd_en_n = 0;
                     end
                 end
                 
                 else if (enable_8) begin
                     cmd_line_n = CMD8; 
-                    cmd_en_n = 1;
+
                     if (timer_50 < 50) begin
                         timer_50_n = timer_50 + 1; // Increment the timer for 50 clock cycles
+                        if(timer_50 < 48) begin
+                            cmd_en_n = 1;
+                        end
                     end else begin
                         timer_50_n = 0; 
                         read_in_40_n = 1; 
                         enable_8_n = 0;
+                        cmd_en_n = 0;
                     end
                 end 
                 
                 else if (enable_55) begin
                     cmd_line_n = CMD55; 
-                    cmd_en_n = 1;
+
                     if (timer_50 < 50) begin
                         timer_50_n = timer_50 + 1; // Increment the timer for 50 clock cycles
+                        if(timer_50 < 48) begin
+                            cmd_en_n = 1;
+                        end
                     end else begin
                         enable_41_n = 1; // Enable the ACMD41 command after 50 clock cycles
                         enable_55_n = 0;
                         timer_50_n = 0;  
+                        cmd_en_n = 0;
                     end
                 end 
                 
                 else if (enable_41) begin 
                     cmd_line_n = ACMD41; 
-                    cmd_en_n = 1; 
+
                     if (timer_50 < 50) begin
                         timer_50_n = timer_50 + 1; // Increment the timer for 50 clock cycles
+                        if(timer_50 < 48) begin
+                            cmd_en_n = 1;
+                        end 
                     end else begin
                         read_in_40_n = 1;
                         enable_41_n = 0; 
                         timer_50_n = 0;
+                        cmd_en_n = 0;
                     end
                 end
             end 
@@ -240,30 +290,45 @@ always_comb begin
                     read_in_timer_n = 0; 
                 end
             end
+            else if(write_en == 1) begin
+                state_n = WRITE;
+            end
         end
         WRITE: begin // Logic for writing data to MISO can be added here
             if(write_en) begin
-                cmd_line_n = CMD12;    
-                cmd_en_n = 1;
-                if(cmd_en) begin
-                    if (index_counter == 48) begin
-                        index_counter_n = 0; // Reset the index counter after sending the command
-                        cmd_en_n = 0;
-                        read_cmd_en_n = 0;
+                if(write_cmd_en) begin
+                    cmd_line_n = CMD12;    
+                    cmd_en_n = 1;
+                    if(cmd_en) begin
+                        if (index_counter == 48) begin
+                            index_counter_n = 0; // Reset the index counter after sending the command
+                            cmd_en_n = 0;
+                            write_cmd_en_n = 0;
+                        end
+                        if (index_counter < 48) begin
+                            index_counter_n = index_counter + 1; // Increment the index counter for each bit
+                        end
+                        mosi = cmd_line[47 - index_counter]; // Shift out the command bit
                     end
-                    if (index_counter < 48) begin
-                        index_counter_n = index_counter + 1; // Increment the index counter for each bit
-                    end
-                    mosi = cmd_line[47 - index_counter]; // Shift out the command bit
+                end
+                else begin
+                    mosi = writebit;
                 end
             end
-            mosi = writebit;
+            else begin
+                state_n = DONE; // Transition to DONE state if not writing
+            end
         end
 
-        DONE: begin
-            // Logic for finalizing the operation can be added here
+        DONE: begin // Logic for finalizing the operation can be added here
+            slave_select = 1;
+            if (warmup_counter < 8) begin
+                warmup_counter_n = warmup_counter + 1; // Warmup counter to stabilize the SD
+            end 
+            else if (warmup_counter == 8) begin
+                finish = 1; // Enable the first bit of the command
+            end
         end
-
         default: state_n = INIT; // Reset to INIT on unexpected state
     endcase
 end
