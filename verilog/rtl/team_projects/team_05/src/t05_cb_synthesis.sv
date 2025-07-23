27c1bdc4b99c884174ea6fb57e188b270f81c64d
Laura Van Ritbergen <lvanritb@ecelnx16.ecn.purdue.edu>
2025-07-23 14:03:27 -0400

    modified:   verilog/dv/team_05/module_tests/t05_cb_synthesis_tb.sv 	modified:   verilog/rtl/team_projects/team_05/src/t05_cb_synthesis.sv 	modified:   verilog/rtl/team_projects/team_05/src/t05_header_synthesis.sv

--- verilog/rtl/team_projects/team_05/src/t05_cb_synthesis.sv
+++ verilog/rtl/team_projects/team_05/src/t05_cb_synthesis.sv
@@ -1,32 +1,33 @@
-typedef enum logic [2:0] {
-    LEFT=0,
-    RIGHT,
-    TRACK,
-    BACKTRACK,
-    FINISH,
-    INIT,
-    SEND
-} state_cb;
-
+`timescale 1ms/10ps
+// typedef enum logic [2:0] {
+//     LEFT=0,
+//     RIGHT,
+//     TRACK,
+//     BACKTRACK,
+//     FINISH,
+//     INIT,
+//     SEND
+// } state_cb;
+  
 module t05_cb_synthesis (
     input logic clk,
     input logic rst,
-    input logic [6:0] max_index, // max index e.g. the top of the tree, given from HTREE MODULE
-    input logic [63:0] h_element, // h_element given to traverse to from SRAM (given curr_index)
-    input logic write_finish, // sent from the header synthesis when all bits of header potion have been written in SPI (then continue to traverse the tree)
-    input logic [3:0] curr_process, // controller state, comment out when testing this module individually
-    output logic char_found, // sent as an enable to the SRAM if a character was found to write path (also to the header synthesis module)
-    output logic [127:0] char_path, // sends the character path to SRAM as data
-    output logic [7:0] char_index, // sends the character index to SRAM as an index to write at correct address
-    output state_cb curr_state, // current internal state of this module in binary tree traversal
-    output logic [6:0] curr_index, // current index of the htree element currently being searched (sent to HTREE MODULE)
-    output logic [127:0] curr_path, // current path in the tree (begin updated as the tree is traversed)
-    output logic [8:0] least1, // least1 of the current htree element
-    output logic [8:0] least2, // least2 of the current htree element
-    output logic [3:0] finished, // finish signal sent to the CONTROLLER
-    output logic [6:0] track_length, // keeps track of current path length to know when to stop tracking the path in the TRACK state
-    output logic [6:0] pos, // keeps track of current position in the path when in TRACK state
-    output logic wait_cycle //waits one clock cycle in transistion between states to allow for output to stabilize
+    input logic [6:0] max_index,
+    input logic [70:0] h_element,
+    input logic write_finish,
+    //input logic [2:0] curr_process,
+    output logic char_found,
+    output logic [127:0] char_path,
+    output logic [7:0] char_index,
+    output state_cb curr_state,
+    output logic [6:0] curr_index,
+    output logic [127:0] curr_path,
+    output logic [8:0] least1,
+    output logic [8:0] least2,
+    output logic [3:0] finished,
+    output logic [6:0] track_length,
+  output logic [6:0] pos,
+  output logic wait_cycle
 );
 
 // next state logic
@@ -37,6 +38,8 @@ logic [6:0] next_track_length; // current path length (for tracking state)
 //logic wait_cycle;
 logic next_wait_cycle;
 logic [6:0] next_pos;
+logic sent;
+logic next_sent;
 
 always_ff @(posedge clk, posedge rst) begin
     if (rst) begin
@@ -46,6 +49,7 @@ always_ff @(posedge clk, posedge rst) begin
         track_length <= 7'b0; // set current path length to 0
         pos <= 7'b1;
         wait_cycle <= 1;
+        sent <= 0;
     end
     else begin
         curr_path <= next_path;
@@ -54,6 +58,7 @@ always_ff @(posedge clk, posedge rst) begin
         curr_index <= next_index;
         pos <= next_pos;
         wait_cycle <= next_wait_cycle;
+        sent <= next_sent;
     end
 end
 
@@ -71,6 +76,7 @@ always_comb begin
         next_track_length = track_length;
         next_pos = pos;
         next_wait_cycle = wait_cycle;
+        next_sent = sent;
 
         case (curr_state)
             INIT: begin 
@@ -113,18 +119,20 @@ always_comb begin
               end
             end
             SEND: begin // state after a character was found and waiting for char bits to be written through SPI
-              if (wait_cycle == 0) begin
+              //if (wait_cycle == 0) begin
                   if (write_finish) begin
                       next_state = BACKTRACK;
                       next_wait_cycle = 1;
+                      //next_sent = 0;
                   end
                   else begin
+                      next_sent = 1;
                       next_state = curr_state;
                   end
-              end
-              else begin
-                next_wait_cycle = 0;
-              end
+              //end
+              // else begin
+              //   next_wait_cycle = 0;
+              // end
             end
             TRACK: begin // after backtrack state when a character was found, use that backtracked path to start from the top of the tree and then retrieve the htree element
               if (wait_cycle == 0) begin
@@ -154,17 +162,22 @@ always_comb begin
                 next_wait_cycle = 1;
                   // if the top of the tree has been reached and left and right have already been traversed, next state is FINISH
                   next_state = state_cb'(track_length < 7'b1 && curr_path[0] == 1'b1 ? FINISH : (curr_path[0] == 1'b1 ? BACKTRACK : TRACK)); 
-                  if (curr_path[0] == 1'b1 && track_length > 0) begin // if last move was right and the number moves is greater than 0 (not at top of tree)
-                      next_path = {1'b0, curr_path[127:1]}; // right shift path to remove last move to "backtrack"
-                      next_state = BACKTRACK;
-                    next_track_length = track_length - 1; // remove one from track length
-                  end
-                  else if (curr_path[0] == 1'b0 && track_length > 0) begin // if not at top and didn't go right but went left
-                      next_state = TRACK;
-                      next_index = max_index;
-                      next_path = {1'b0, curr_path[127:1]};
-                      next_track_length = track_length - 1;
-                  end
+                    if (track_length == 1'b1 && !sent) begin //  once top of tree is about to be reached send 0's accumulated to be written and then return to backtrack again
+                          next_state = SEND;
+                          next_sent = 1; // set sent to 1 so when coming back to this state it doesn't go to sent again
+                    end
+                    else if (curr_path[0] == 1'b1 && track_length > 0) begin // if last move was right and the number moves is greater than 0 (not at top of tree)
+                          next_sent = 0; // once SEND is done and state is backtrack again, set sent to 0 and remove the last move to reach the top of the tree
+                          next_path = {1'b0, curr_path[127:1]}; // right shift path to remove last move to "backtrack"
+                          next_state = BACKTRACK;
+                          next_track_length = track_length - 1; // remove one from track length
+                    end
+                    else if (curr_path[0] == 1'b0 && track_length > 0) begin // if not at top and didn't go right but went left
+                        next_state = TRACK;
+                        next_index = max_index;
+                        next_path = {1'b0, curr_path[127:1]};
+                        next_track_length = track_length - 1;
+                    end
               end
               else begin
                 next_wait_cycle = 0;
@@ -175,7 +188,6 @@ always_comb begin
                   next_track_length = track_length + 1; // update total path length
                   next_wait_cycle = 1;
                   next_state = state_cb'((least2[8] == 1'b0) ? SEND : LEFT);
-
                   if (least2[8] == 1'b0 || least2 == 9'b110000000) begin // if RSE is a char or there is no chracter
                       if (least2 != 9'b110000000) begin
                           char_index = least2[7:0]; // set output character (index) to LSE, NOT to tracking index
--- verilog/rtl/team_projects/team_05/src/t05_header_synthesis.sv
+++ verilog/rtl/team_projects/team_05/src/t05_header_synthesis.sv
@@ -1,43 +1,31 @@
 module t05_header_synthesis (
     input logic clk,
     input logic rst,
-<<<<<<< HEAD
-    // from cb synthesis
     input logic [7:0] char_index,
     input logic char_found,
     input logic [8:0] least1,
     input logic [8:0] least2,
-    // internal
+    input logic [127:0] char_path,
+    input logic [6:0] track_length,
     output logic [8:0] header,
-    // output to SPI (write enable and bit data)
     output logic enable,
     output logic bit1,
-    // output to cb synthesis (write_finish)
     output logic write_finish
-=======
-    input logic [7:0] char_index, // index (character) found from cb synthesis module
-    input logic char_found, // from cb synthesis module to indicate a char was found
-    input logic [8:0] least1, // least 1 of the htree element of which the character was found from cb synthesis
-    input logic [8:0] least2, // least 2 of the htree element of which the character was found from cb synthesis
-    output logic [8:0] header, // portion of the header (1 followed by the character) created within this module
-    output logic enable, // waits one cycle after character was found to decide how many zeroes to add to the end of the char
-    output logic bit1, // bit sent to the SPI
-    output logic write_finish // sent to the cb synthesis to indicate all bits of header portion were written
->>>>>>> fe4c62853e320c7aef3d9fb01a76c14abb9f1fca
 );
-logic char_added; // char added will be high once char is found
-logic [3:0] zeroes; // number of zeroes to add after character bits are written
-logic [3:0] count; // counter used to keep track of the correct number of zeroes sent
-
-// next state logic
-logic [8:0] next_header; 
-logic [3:0] next_count;
-logic next_bit1;
+logic char_added;
+logic [8:0] next_header;
+logic [7:0] zeroes;
+logic [7:0] next_zeroes;
 logic next_enable;
-logic [3:0] next_zeroes;
+logic [7:0] count;
+logic [7:0] next_count;
+logic next_bit1;
 logic next_char_added;
 logic next_write_finish;
 logic start;
+logic next_write_zeroes;
+logic write_zeroes;
+logic next_start;
 
 always_ff @(posedge clk, posedge rst) begin
     if (rst) begin
@@ -48,6 +36,8 @@ always_ff @(posedge clk, posedge rst) begin
         bit1 <= 0;
         char_added <= 0;
         write_finish <= 0;
+        write_zeroes <= 0;
+        start <= 0;
     end
     else begin
         header <= next_header;
@@ -57,6 +47,8 @@ always_ff @(posedge clk, posedge rst) begin
         bit1 <= next_bit1;
         char_added <= next_char_added;
         write_finish <= next_write_finish;
+        write_zeroes <= next_write_zeroes;
+        start <= next_start;
     end
 end
 
@@ -67,78 +59,96 @@ always_comb begin
     next_count = count;
     next_bit1 = bit1;
     next_char_added = char_added;
-    next_write_finish = 0;
+    next_write_finish = write_finish;
+    next_write_zeroes = write_zeroes;
+    next_start = start;
 
     if ((char_found == 1'b1)) begin
         next_header = {1'b1, char_index}; // add control bit, beginning 1, and character index for header
         next_char_added = 1;
         next_zeroes = 0;
-        start = 0;
-    end
-    else if (char_added == 1'b1) begin
-        if (least1[8] == 1'b0 && least2[8] == 1'b0 && header[7:0] == least2[7:0]) begin // if both least1 and least2 are characters and state is backtrack (both chars have been found)
-            next_zeroes = 4'b0010; // add 2 zeroes
-        end
-        else if ((least1[8] == 1'b1 && least2[8] == 1'b0) || (least2[8] == 1'b1 && least1[8] == 1'b0)) begin  // if only either least 1 or least 2 is a sum and the other is a character
-<<<<<<< HEAD
-            //next_header = {header[9:0], 1'b0}; // add one ending 0 for one character
-            next_zeroes = 4'b1; // add 1 zero
-        end
-        else begin // if only left char was found and not the right char
-            next_zeroes = 4'b0; // add 0 zeroes
-=======
-            next_zeroes = 4'b1;
-        end
-        else begin
-            next_zeroes = 4'b0; // otherwise, only the left character was found, and the right char hasn't been found, so don't add zeroes to this char
->>>>>>> fe4c62853e320c7aef3d9fb01a76c14abb9f1fca
-        end
-        next_char_added = 1'b0;
-        start = 1; // when start is 1, set enable high
-    end
-    else begin
-        start = 0;
-        next_char_added = 1'b0;
-    end
-<<<<<<< HEAD
-    if (start) begin // enable (start writing bits)
+        next_start = 0;
         next_enable = 1;
-        start = 0;
+        next_start = 1;
+        next_write_finish = 0;
+        //if (track_length == 1) begin // if there is a character found at the top of the tree
+            next_zeroes = zeroes + 1;
+        //end
+    end
+    // else if (char_added == 1'b1) begin
+    //     // if (least1[8] == 1'b0 && least2[8] == 1'b0 && header[7:0] == least2[7:0]) begin // if both least1 and least2 are characters and state is backtrack (both chars have been found)
+    //     //     next_zeroes = 4'b0010;
+    //     // end
+    //     // else if ((least1[8] == 1'b1 && least2[8] == 1'b0) || (least2[8] == 1'b1 && least1[8] == 1'b0)) begin  // if only either least 1 or least 2 is a sum and the other is a character
+    //     //     //next_header = {header[9:0], 1'b0}; // add one ending 0 for one character
+    //     // else begin
+    //     //     next_zeroes = 4'b0;
+    //     // end
+    //     //next_char_added = 1'b0;
+    //     next_start = 1;
+    // end
+    // else begin
+    //     next_start = 0;
+    //     //next_char_added = 1'b0;
+    // end
+    // if ((char_path[0] == 1'b1) && (track_length > 7'b1)) begin // already explore both left and right and a character was already found and added to the header
+    //         next_zeroes = zeroes + 8'b1;
+    //         next_write_zeroes = 0;
+    // end
+    if ((track_length == 1)) begin
+        next_write_zeroes = 1;
+        next_enable = 1;
+        next_write_finish = 0;
     end
 
-    if (enable && (count < 9)) begin // send the bits of leading 1 and found char
-=======
-    if (start) begin // set enable high
+    if (start) begin
         next_enable = 1;
-        start = 0;
-    end
-    // shift register logic
-    if (enable && (count < 9)) begin // while leading 1 and char are being added
->>>>>>> fe4c62853e320c7aef3d9fb01a76c14abb9f1fca
+        next_start = 0;
         next_bit1 = header[8];
-        next_header = {header[7:0], 1'b0}; // shift out msb to write
+        next_header = {header[7:0], 1'b0}; // shift out msb to write (first occurrence)
         next_count = count + 1;
+        next_char_added = 1;
     end
-<<<<<<< HEAD
-    else if (enable && count >= 9) begin // send 0 bits (if any)
-        if (count < (zeroes + 9)) begin
+
+    if (enable && char_added) begin
+        if (count < 9) begin
+            next_bit1 = header[8];
+            next_header = {header[7:0], 1'b0};
+            next_count = count + 1;
+        end
+        else begin
+            next_count = 0;
+            //if (!write_zeroes) begin
+                next_enable = 0;
+                next_write_finish = 1;
+                next_bit1 = 0;
+                next_count = 0;
+                next_char_added = 0;
+            //end
+        end
+    end
+    else if (enable && write_zeroes) begin
+        if (count < (zeroes)) begin
             next_count = count + 1;
             next_bit1 = 1'b0;
         end
-        else begin // otherwise set enables low and write_finish to cb synthesis high
-=======
-    else if (enable && count >= 9) begin // if count is greater than 9 (leading one and 8 bits of char have been written)
-        if (count < (zeroes + 9)) begin // this will add either 0, 1, or 2 zeroes depending on states described above
-            next_count = count + 1;
-            next_bit1 = 1'b0;
-        end
-        else begin // once all zeroes have been added, set all enables low and indicate that write has been finished to cb synthesis
->>>>>>> fe4c62853e320c7aef3d9fb01a76c14abb9f1fca
+        else begin
             next_bit1 = 1'b0;
             next_enable = 0;
             next_count = 0;
             next_write_finish = 1;
+            next_write_zeroes = 0;
+            next_zeroes = 0;
+            next_char_added = 0;
         end
+    end
+    else begin
+        next_bit1 = 1'b0;
+        //next_enable = 0;
+        next_count = 0;
+        //next_write_finish = 1;
+        //next_write_zeroes = 0;
+        //next_char_added = 0;
     end
 
 end
